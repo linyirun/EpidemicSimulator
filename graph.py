18f4da9b4cced942a61e1ea558d9890638447479
Bill Lin <bill10391@gmail.com>
2023-03-31 22:18:23 -0400

    fixed runtime error bug
    was mutating set while iterating through it

--- graph.py
+++ graph.py
@@ -21,17 +21,21 @@ class Graph:
         person2.family[person1.id] = edge
 
     def update_edge(self, current_frame, recover_period: int, close_contact_distance: int):
+        to_remove = set()
         for patient in self.infected:
             patient.close_contact = {}
             if current_frame - patient.infection_frame > recover_period:
                 patient.state = RECOVERED
-                self.infected.remove(patient)
+                to_remove.add(patient)
                 self.recovered.add(patient)
             else:
                 for person in self.susceptible:
                     if ((person.location[0] - patient.location[0]) ** 2 + (
                             person.location[1] - patient.location[1]) ** 2) ** 0.5 < close_contact_distance:
                         patient.create_close_contact_edge(person)
+
+        for patient in to_remove:
+            self.infected.remove(patient)
 
     def make_infection(self, close_contact_distance: int) -> set[Person]:
         """return all the newly infected people"""
